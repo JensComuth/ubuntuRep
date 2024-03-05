cd2a085e29db3d946e624bcd7f8fcf0ab0d195ea
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-03-05 11:43:19 +0100

    mm yes edit date + name

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Git Oefen Opdracht</title>
+</head>
+<body>
+<div class="container">
+
+
+    
+    <div class="jumbotron">
+    <h1>Git Oefenopdracht</h1><br>
+   
+    <h2>
+        Actuele datum en tijd
+        <?php
+        echo "Today is " . date("Y/m/d") . "<br>";
+        echo "The time is " . date("h:i:sa");
+        ?>
+    </h2><br>
+
+    <p>
+        Jens Comuth
+    </p>
+
+    </div>
+    <img src="https://image.parool.nl/241172775/feature-crop/2480/1653/max-verstappen-het-racen-is-leuk-maar-alles-eromheen-gaat" class="img-rounded" alt="Git Deployment">
+</div>
+</body>
+</html>
